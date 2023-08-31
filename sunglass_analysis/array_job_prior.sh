7304ddef53c3514cbc805c571c597fac3fad991c
tlmakinen <lucas.makinen@gmail.com>
2023-08-31 15:19:18 +0200

    shell scripts for slow sunglass code

---
+++ sunglass_analysis/array_job_prior.sh
@@ -0,0 +1,36 @@
+#PBS -N sunglass_prior
+#PBS -q batch
+#PBS -l walltime=3:00:00
+#PBS -t 0-1000%100
+#PBS -j oe
+#PBS -o ${HOME}/data/jobout/sunglass/prior/${PBS_JOBNAME}.${PBS_JOBID}.o
+
+#PBS -l nodes=1:ppn=1,walltime=3:00:00,mem=32GB
+# do not use PBS -l nodes=1:ppn=64,mem=128GB
+
+echo cd-ing...
+cd /home/makinen/repositories/alfi_lensing/sunglass_analysis/
+
+echo activating environment...
+
+module purge
+module load cuda/11.8
+module load gcc/11.3.0
+module load cmake
+module load inteloneapi/2023.0
+module load intelpython/3-2022.2.1
+
+ 
+XLA_FLAGS=--xla_gpu_cuda_data_dir=\${CUDA_PATH}
+export XLA_FLAGS
+ 
+source /home/makinen/venvs/fastjax/bin/activate
+
+echo running script...
+echo "arrayind is ${PBS_ARRAYID}"
+python run_sunglass.py prior ${PBS_ARRAYID}
+
+echo done...
+
+
+exit 0
